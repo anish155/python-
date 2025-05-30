cd4ff64a797bac50648b3220004957a3a6c11ef4
anish155 <anishtandukar3@gmail.com>
2025-05-30 20:42:52 +0545

    manipulation of index in loop

--- layered.py
+++ layered.py
@@ -1,3 +1,10 @@
+"""sentence="python is fun"
+for x in sentence.split(" "):
+    print(x)"""
+'''sentence="python is fun"
+new_sentence = sentence.replace(" ", "_")
+print(new_sentence)'''
+
 sentence="python is fun"
 for x in sentence.split(" "):
-    print(x)
+    print(x[::-1])
