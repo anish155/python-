b3960cf7fc9af2be172ad0ab7c9c813fd887f636
anish155 <anishtandukar3@gmail.com>
2025-06-03 17:12:20 +0545

    beginning search

---
+++ Linear.py
@@ -0,0 +1,11 @@
+def simple_search(arr,target):
+    tries=4
+    for i in range(len(arr)):
+        if arr[i]==target:
+            print("the number was",arr[i])
+            break
+        else:
+            print("the guessed number is wrong")
+            tries-=1
+        print("you have this much tries left:",tries)
+simple_search([10,12,34,56,100],56)
