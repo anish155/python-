2ae523879a322763bdfd646576339d53202f9ef7
anish155 <anishtandukar3@gmail.com>
2025-06-03 14:46:55 +0545

    finding the largets number.

--- maximum.py
+++ maximum.py
@@ -1,6 +1,9 @@
 num_arr=[10,12,30,45,56]
-large_num=num_arr[0]
+'''large_num=num_arr[0]
 for num in num_arr:
     if num>large_num:
         large_num=num
-print(large_num)
+print(large_num)'''
+print(max(num_arr))
+print(min(num_arr))
+print(sum(num_arr)/len(num_arr))
