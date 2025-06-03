5b0d22ea39461e2023d87b64ab27bd65b8a311e1
anish155 <anishtandukar3@gmail.com>
2025-06-03 14:41:27 +0545

    changing the cases

---
+++ lowertoupper.py
@@ -0,0 +1,8 @@
+def convert_to_lower(word):
+    lowcaseword=word.lower()
+    print(lowcaseword)
+def convert_to_upper(word):
+    uppercaseword=word.upper()
+    print(uppercaseword)
+convert_to_upper("hi")
+convert_to_lower("HI")
