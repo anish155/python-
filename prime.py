e277c89bb3e6f9e9a9170cf81dcc1dc812bac6e9
anish155 <anishtandukar3@gmail.com>
2025-06-04 20:17:36 +0545

    adding user input

--- prime.py
+++ prime.py
@@ -1,4 +1,4 @@
-def prime(num):
+'''def prime(num):
     if num<2:
         print("it is not a prime number.")
         return
@@ -7,4 +7,19 @@ def prime(num):
             print(num,"this is not prime number")
             return
     print(num,"this is a prime number.")
-prime(12)
+prime(12)'''
+
+def Input():
+    num=int(input("Enter the number:"))
+    return num
+def Check(num):
+    if(num<2):
+        print(num,"the number is not a prime number. ")
+    for i in range(2,int(num**0.5)+1):
+        if(num%i==0):
+            print(num,"the number is not a prime.")
+        else:
+            print(num,"this number is a prime number.")
+    return num
+num=Input()
+Check(num)
