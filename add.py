8503ffc48815268baf898ae6c5c316414a3af103
anish155 <anishtandukar3@gmail.com>
2025-06-09 19:24:14 +0545

    adding new key and value

---
+++ add.py
@@ -0,0 +1,9 @@
+person={"name":"",
+        "age":"",
+        "city":""}
+person["name"]="Anish Tandukar"
+person["age"]=20
+person["city"]="kathmandu"
+print(person)
+person.update({"work":"coding"})
+print(person)
