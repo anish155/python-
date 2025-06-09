d39db70b672be003c3575123b161bec60d69149a
anish155 <anishtandukar3@gmail.com>
2025-06-09 19:25:32 +0545

    looping in dictionary

---
+++ loop.py
@@ -0,0 +1,8 @@
+person={"name":"",
+        "age":"",
+        "city":""}
+person["name"]="Anish Tandukar"
+person["age"]=20
+person["city"]="kathmandu"
+for key,value in person.items():
+    print(key,":",value)
