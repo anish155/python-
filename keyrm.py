f95a60160712b1fa0d3dccb508bbf0b2acc554be
anish155 <anishtandukar3@gmail.com>
2025-06-09 19:25:07 +0545

    removing age item from dict

---
+++ keyrm.py
@@ -0,0 +1,9 @@
+person={"name":"",
+        "age":"",
+        "city":""}
+person["name"]="Anish Tandukar"
+person["age"]=20
+person["city"]="kathmandu"
+print(person)
+del person["age"]
+print(person)
