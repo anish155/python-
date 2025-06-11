ae8112f13e2f86178016dd531f1a14f03408f1e0
anish155 <anishtandukar3@gmail.com>
2025-06-11 17:24:27 +0545

    pandas table

---
+++ table.py
@@ -0,0 +1,7 @@
+import pandas as pd
+person={"name":["anish","rahul","dinesh","pujan","hari","kamal"],
+        "age":[20,15,14,19,25,30],
+        "job":["frontend","database","backend","designer","frontend","designer"],
+        "Salary":[20000,18000,21000,15000,19000,15000]}
+df=pd.DataFrame(person)
+print(df)
