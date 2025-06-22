d59b3c3d44eabb9e44f747d937f5b6e9df8aaa0d
anish155 <anishtandukar3@gmail.com>
2025-06-22 17:51:56 +0545

    simple search

---
+++ 6-22-2025/linear.py
@@ -0,0 +1,6 @@
+lst=[100,40,23,67,98,56,39,10,5,8,1,1000,25000,670,333,99999]
+target=1
+if target in lst:
+    print(f"the number was found on index: {lst.len(target)}")
+else:
+    print("didnot find the target")
