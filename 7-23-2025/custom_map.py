972df2998c2e5fa64fc0d04578d6664f6d6843fe
anish155 <anishtandukar3@gmail.com>
2025-07-23 14:31:42 +0545

    trying built in map

--- 7-23-2025/custom_map.py
+++ 7-23-2025/custom_map.py
@@ -1,12 +1,16 @@
-def my_map(func,iterate):
-    result=[]
-    for items in iterate:
-        result.append(func(items))
-    return result
+# def my_map(func,iterate):
+#     result=[]
+#     for items in iterate:
+#         result.append(func(items))
+#     return result
 
-def sq_numbers(nums):
-    return nums*nums
+# def sq_numbers(nums):
+#     return nums*nums
+
+# nums=[1,2,3,4]
+# squaring=my_map(sq_numbers,nums)
+# print(squaring)
 
 nums=[1,2,3,4]
-squaring=my_map(sq_numbers,nums)
-print(squaring)
+squaring=map(lambda x: x*x,nums)
+print(list(squaring))
