d8a999630be7132d89982709ae7f9eb9c7bb3666
anish155 <anishtandukar3@gmail.com>
2025-08-21 17:34:54 +0545

    diamond

---
+++ 8-21-2025/diamond.py
@@ -0,0 +1,19 @@
+def diamond(n):
+    for i in range(1,1+n):
+        print(" " * (n - i), end="")
+        for j in range(1,i+1):
+            print(j,end="")
+        for j in range(i-1,0,-1):
+            print(j,end="")
+        print()
+    
+    for i in range(n-1,0,-1):
+        print(" " * (n - i), end="")
+        for j in range(1,i+1):
+            print(j,end="")
+        for j in range(i-1,0,-1):
+            print(j,end="")
+        print()
+
+
+diamond(3)
