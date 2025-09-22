922e0901f5236b66f0b747f2043d5bc6f9a355ba
anish155 <anishtandukar3@gmail.com>
2025-09-22 21:16:08 +0545

    counting letters

--- week1/count.py
+++ week1/count.py
@@ -1,6 +1,23 @@
-word='the man who is the young is the the the the'
-count=0
-for x in word.split(" "):
-    if x=='the':
-        count+=1
-print(count)
+# word='the man who is the young is the the the the'
+# count=0
+# for x in word.split(" "):
+#     if x=='the':
+#         count+=1
+# print(count)
+
+class counter:
+    def Take(self):
+        senmtence=input("Enter the sentences:")
+        return senmtence
+    
+    def count(self,letters):
+        count=0
+        for x in letters.split(" "):
+            if x:
+                count+=1
+        print(count)
+
+
+counting=counter()
+words=counting.Take()
+counting.count(words)
