d9cdb1be9e025697780415d7cb3b31b8cd3d3770
anish155 <anishtandukar3@gmail.com>
2025-09-23 18:16:32 +0545

    replacing elements

--- week1/layered.py
+++ week1/layered.py
@@ -5,6 +5,17 @@ for x in sentence.split(" "):
 new_sentence = sentence.replace(" ", "_")
 print(new_sentence)'''
 
-sentence="python is fun"
-for x in sentence.split(" "):
-    print(x[::-1])
+# sentence="python is fun"
+# for x in sentence.split(" "):
+#     print(x[::-1])
+
+def Take():
+    sentence=input("Enter the word:")
+    return sentence
+
+def Replacing(words):
+    new_word=words.replace(" ","_")
+    print(new_word)
+
+words=Take()
+Replacing(words)
